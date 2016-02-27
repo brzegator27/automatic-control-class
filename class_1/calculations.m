860143c6fc05e450f6df710d32b952fff341ef33
Jakub Brzegowski <symfiz@gmail.com>
2016-02-27 14:52:38 +0100

    Added some calculations to class_1

--- class_1/calculations.m
+++ class_1/calculations.m
@@ -1,8 +1,63 @@
 % read in constants from data.m
 data;
 
-% transmittance:
+% transmittance
+
 % we've got
 % G(s) = 1 / (M * s^2 + alfa * s + c), so
 numerator = [0 0 1];
-denominator = [M damperConst springConst];
+denominator = [massConst damperConst springConst];
+
+step(numerator, denominator);       % step response of dynamic system
+                                    % more about setp function on
+                                    % uk.mathworks.com/help/control/ref/step.html
+                                    
+impulse(numerator, denominator);    % impulse response of dynamic system
+                                    % more about setp function on
+                                    % uk.mathworks.com/help/control/ref/impulse.html
+                                    
+% we can also create real- or romplex-valued transfer function model 
+% (TF object) and e.g. pass it to step or impulse function. 
+% More about tf function on uk.mathworks.com/help/control/ref/tf.html
+object = tf(numerator, denominator);
+
+step(object);
+impulse(object);
+
+% we can show object properties using get function
+get(object);
+
+
+
+% in accordance with uk.mathworks.com/help/signal/ref/tf2zp.html, given a
+% SIMO continuous-time system in polynomial transfer function form we caon
+% use output of tf2zp to produce the single-input, multi-output (SIMO)
+% factored transfer function form.
+fvtool(numerator, denominator, 'polezero'); % opens FVTool and displays the 
+                            % magnitude response of the digital filter defined
+                            % with numerator and denominator. 
+                            % More about this function on 
+                            % uk.mathworks.com/help/signal/ref/fvtool.html#zmw57dd0e62094
+                            
+[num den] = eqtflength(numerator, denominator);
+[zeros poles gain] = tf2zp(num, den);   % transfer function to zero-pole
+                            % More about this function on 
+                            % uk.mathworks.com/help/signal/ref/tf2zp.html
+                            
+text(real(zeros) + .1, imag(zeros), 'Zero');
+text(real(poles) + .1, imag(poles), 'Pole');    % function text add text
+                            % descritptions to data points
+                            % more on uk.mathworks.com/help/matlab/ref/text.html
+
+% we can also plot zeros and poles using pzmap function
+pzmap(numerator, denominator);
+pzmap(poles, zeros);
+pzmap(object);              % more on uk.mathworks.com/help/control/ref/pzmap.html
+
+grid on
+
+
+
+
+
+
